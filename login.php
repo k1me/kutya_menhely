7acf830f01e36fdcb199d54fb4ea5f7fdef94a3d
k1me <m.kiraly21@proton.me>
2023-04-09 15:52:18 +0200

    a belépés most már rendesen működik

--- login.php
+++ login.php
@@ -1,4 +1,42 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "kutyamenhely";
+$errors = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $uname = $_POST['uname'];
+    $passwd = $_POST['passwd'];
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Sikertelen csatlakozás: " . $conn->connect_error);
+    }
+    $hashed = hash('sha256', $passwd);
+
+    $sql = "SELECT * FROM users WHERE uname = '$uname'";
+    $res = $conn->query($sql);
+
+    if ($res->num_rows > 0) {
+        $row = $res->fetch_assoc();
+        if($hashed === $row['passwd']) {
+            $siker = TRUE;
+            $_SESSION['uname'] = $row['uname'];
+            $_SESSION['passwd'] = $row['passwd'];
+            $conn->close();
+            header('Location: index.php');
+            exit();
+        } else {
+            $errors[] = 'Hibás felhasználónév vagy jelszó!';
+            $siker = FALSE;
+        }
+    } else {
+        $errors[] = 'Hibás felhasználónév vagy jelszó!';
+        $siker = FALSE;
+    }
+    $conn->close();
+}
 $title = 'Belépés';
 $page = 'login';
 ?>
@@ -9,9 +47,18 @@ $page = 'login';
     <?php include 'header.html'; ?>
     <?php include 'navbar.php'; ?>
     <div class="form-wrapper">
-        <form method="POST">
+        <form action="login.php" method="POST" >
             <h2 id="login_h">belépés</h2>
             <img src="img/login_avatar.png" alt="login-avatar" class="avatar">
+            <?php 
+            if (isset($siker) && $siker === TRUE) {
+                echo "<p>Sikeres regisztráció!</p>";
+            } else {
+                foreach ($errors as $error) {
+                    echo "<p>$error</p>";
+                }
+            }
+            ?>
             <input type="text" name="uname" placeholder="Felhasználónév">
             <input type="password" name="passwd" placeholder="Jelszó" required>
             <button type="submit">Belépés</button>
