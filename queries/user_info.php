d99ac00d79fd0c85b34970c07921f3e5a637c709
k1me <m.kiraly21@proton.me>
2023-04-10 20:19:46 +0200

    felhasználó adatainak kiíratása

---
+++ queries/user_info.php
@@ -0,0 +1,26 @@
+<?php
+include 'queries/db_connect.php';
+$sql = "SELECT * FROM user_info WHERE uname = '{$_SESSION["uname"]}'";
+$res = $conn->query($sql);       
+$row = $res->fetch_assoc();                 
+echo '<img src="';
+if ($row['pfp'] !== NULL) {
+    echo '$row["pfp"]"';
+} else {
+    echo 'img/login_avatar.png"';
+    echo ' alt="profil-avatar" class="avatar">';
+} 
+echo '<div class="user-info">';
+    echo "<h3>Felhasználónév:</h3>";
+    echo "<p>{$row['uname']}</p>";
+    echo "<h3>Vezetéknév:</h3>";
+    echo "<p>{$row['last_name']}</p>";
+    echo "<h3>Keresztnév:</h3>";
+    echo "<p>{$row['first_name']}</p>";
+    echo "<h3>Email:</h3>";
+    echo "<p>{$row['email']}</p>";
+    echo "<h3>Születési dátum</h3>";
+    echo "<p>{$row['date_of_birth']}</p>";
+    $conn->close();
+echo "</div>";
+?>
