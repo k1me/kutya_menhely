626404f45f338b9e538e3a4d5c4ea268899c632c
k1me <m.kiraly21@proton.me>
2023-04-11 20:02:04 +0200

    avatár megjelenítése

--- queries/user_info.php
+++ queries/user_info.php
@@ -3,12 +3,10 @@ include 'queries/db_connect.php';
 $sql = "SELECT * FROM user_info WHERE uname = '{$_SESSION["uname"]}'";
 $res = $conn->query($sql);       
 $row = $res->fetch_assoc();                 
-echo '<img src="';
-if ($row['pfp'] !== NULL) {
-    echo '$row["pfp"]"';
+if ($row['pfp'] !== NULL && $row['pfp'] !== "") {
+    echo '<img src="img/avatar/'.$row['pfp'].'" alt="profil-avatar" class="avatar"/>';
 } else {
-    echo 'img/login_avatar.png"';
-    echo ' alt="profil-avatar" class="avatar">';
+    echo '<img src="img/login_avatar.png" alt="profil-avatar" class="avatar"/>';
 } 
 echo '<div class="user-info">';
     echo "<h3>Felhasználónév:</h3>";
